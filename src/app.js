0ac83aaf394d70f947eeb622de4f8db68a2de8a5
longyang <cool2434681882@outlook.com>
2019-04-04 01:01:26 +0800

    中场休息

--- src/app.js
+++ src/app.js
@@ -2,17 +2,23 @@
 import express from 'express'//es6
 const app = express()
 
-// app.set('views','')
+import config from './config'
+
+//开放静态资源
+app.use('/node_modules',express.static(config.node_modules_path))
+app.use('/public',express.static(config.public_path))
+
+//配置模板ejs引擎
+app.set('views',config.viewPath)
+app.set('view engine', 'ejs')
 
 //配置bebal
 
 app.get('/',(req,res) => {
     // res.end('hello!!!!')
-    res.render('')
+    res.render('index')
 })
-// app.listen(3000,() => {
-//     console.log('runing in 3000')
-// })
+app.listen(3000,() => {
+    console.log('runing in 3000')
+})
 
-import {foo} from './config'
-console.log(foo)
--- src/config.js
+++ src/config.js
@@ -4,6 +4,25 @@
 // export const f = function(){}
 
 //写法2
-const foo = 'bar'
-const f = function(){}
-export {foo,f}
+// const foo = 'bar'
+// const f = function(){}
+// export {foo,f}
+
+// 获取
+// import {foo} from './config'
+// console.log(foo)
+//获取全部
+// import * as config from './config'
+// console.log(config)
+
+//import config from './config' 会加载export default 导出的文件 = module.export=fn
+
+//通过export 导出的成员必须通过解构赋值按需加载 或者* as 整体加载
+import { join } from 'path'
+export default {
+    viewPath: join(__dirname,'../views'),
+    node_modules_path: join(__dirname,'../node_modules'),
+    public_path: join(__dirname,'../public')
+}
+
+
