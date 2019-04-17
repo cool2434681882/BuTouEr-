b37388c06094d5b7a7e11b85daa4d5f14fec86fb
longyang <cool2434681882@outlook.com>
2019-04-17 23:34:02 +0800

    休息

--- src/controllers/advert.js
+++ src/controllers/advert.js
@@ -1,10 +1,19 @@
 import Advert from '../models/advert'
 
 export function showAdvertList(req,res,next){
-    Advert.find((err,adverts)=>{
-        if(err) return next(err)
-        res.render('advert_list.html',{ adverts })
-    })
+    // Advert.find((err,adverts)=>{
+    //     if(err) return next(err)
+    //     res.render('advert_list.html',{ adverts })
+    // })
+    const page = Number.parseInt(req.query.page)
+    Advert
+        .find()
+        .skip((page - 1) * 5)
+        .limit(5)
+        .exec((err,adverts)=>{
+            if(err) next(err)
+            res.render('advert_list.html',{ adverts })
+        })
 }
 
 export function showAdvertAdd(req,res,next){
